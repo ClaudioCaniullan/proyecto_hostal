ada3079fa87fafc0effee12a2f2a5431e935f303
claudio.caniullan <claudio.caniullan@usach.cl>
2021-07-20 23:51:06 -0400

    creacion de super usuario, creacion e implementación de index y statics

--- hostal/hostal/settings.py
+++ hostal/hostal/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'hostal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # django busca los templates en la carpeta del mismo nombre
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# indicamos a Django donde buscar los static
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
---
+++ hostal/hostal/views.py
@@ -0,0 +1,5 @@
+# Django
+from django.shortcuts import render, redirect
+
+def index(request):
+	return render(request,'index.html', {})
